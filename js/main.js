5b1b3a7431e76df46680da9a4cd8236b1eab102f
Rakib0061 <rakibulislam0061@gmail.com>
2024-03-09 16:15:47 +0600

    solve multi audio canvas

--- js/main.js
+++ js/main.js
@@ -61,100 +61,82 @@ function handleCollapse(params) {
 // ✅hamburger manu link's collapse  end
 
 // ✅Audio Visualizer canvas draw start
-const audio = new Audio("../public/audio/Podcast.mp3");
+const btnAudio = document.querySelectorAll(".btnAudio");
+const audioPlay = document.querySelectorAll(".fa-play");
+const audioPause = document.querySelectorAll(".fa-headphones");
+const audioSrc = [
+  "../public/audio/Podcast.mp3",
+  "../public/audio/Podcast2.mp3",
+];
+const audio = audioSrc.map((src) => new Audio(src));
+const audioCtx = audioSrc.map(() => new AudioContext());
+const audioAnalyzer = audioCtx.map((ctx) => ctx.createAnalyser());
 const canvas = document.querySelectorAll("canvas");
-const btnAudioPlay = document.querySelectorAll(".btnAudioPlay");
-const headphones_icon = document.querySelector(".fa-headphones");
-const play_icon = document.querySelector(".fa-play");
-const audioCtx = new AudioContext();
 const canvasCtx = Array.from(canvas).map((canvas) => canvas.getContext("2d"));
-let audioSource, audioAnalyzer, bufferLength, dataArray;
+let audioSource, bufferLength, dataArray;
 
-function playPaseBtn() {
-  headphones_icon.classList.toggle("d-none");
-  play_icon.classList.toggle("d-none");
+// audi button toggle
+function handleBtnToggle(indx) {
+  if (audioPause[indx].classList.contains("d-none")) {
+    audioPause[indx].classList.remove("d-none");
+    audioPlay[indx].classList.add("d-none");
+    audio[indx].play();
+  } else {
+    audioPause[indx].classList.add("d-none");
+    audioPlay[indx].classList.remove("d-none");
+    audio[indx].pause();
+  }
 }
-// toggel button state fucntion end
 
-// get extra info from audio file start
-function audioInfo() {
-  audioSource = audioCtx.createMediaElementSource(audio);
-  // open analyzer method
-  audioAnalyzer = audioCtx.createAnalyser();
-  // connect analyzer to audio source
-  audioSource.connect(audioAnalyzer);
-  // connect analyzer to destination (speakers)
-  audioAnalyzer.connect(audioCtx.destination);
-  // get fft size (how much width the single audio wave )
-  audioAnalyzer.fftSize = 2048;
-  // bufferLength (half of fft size it'san read only property)
-  bufferLength = audioAnalyzer.frequencyBinCount;
-  // store frequency data a special type array (init it will be 0)
-  dataArray = new Uint8Array(bufferLength);
+// audio src analazation
+function handleAnalization(indx) {
+  if (!audioSource) {
+    audioSource = audioCtx[indx].createMediaElementSource(audio[indx]);
+    audioSource.connect(audioAnalyzer[indx]);
+    audioAnalyzer[indx].connect(audioCtx[indx].destination);
+    audioAnalyzer[indx].fftSize = 2048;
+    bufferLength = audioAnalyzer[indx].frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+    audioSource = undefined;
+  }
 }
-// get extra info from audio file end
 
-// canvas drawing start
-function drawCanvas() {
-  const barwidth = 2;
+function handleDraw(indx) {
+  const barWidth = 2;
   const barGap = 5;
   let x, y, barHeight;
 
   function animate() {
     x = 0;
-    audioAnalyzer.getByteFrequencyData(dataArray);
-    console.log(dataArray);
-    canvasCtx.map((ctx) =>
-      ctx.clearRect(
-        0,
-        0,
-        canvas.forEach((cnvs) => cnvs.width),
-        canvas.forEach((cnvs) => cnvs.height)
-      )
-    );
+    audioAnalyzer[indx].getByteFrequencyData(dataArray);
+    canvasCtx[indx].clearRect(0, 0, canvas[indx].width, canvas[indx].height);
 
     for (let index = 0; index < bufferLength; index++) {
-      // data array update hoitcea na tai loop choltcea na
       barHeight = dataArray[index];
-      y = canvas.forEach((cnvs) => cnvs.width) - barHeight;
-      canvasCtx.map((ctx) => ctx.fillRect(x, y, barwidth, barHeight));
+      y = canvas[indx].height - barHeight / 2;
 
-      x += barwidth + barGap;
+      canvasCtx[indx].fillStyle = `rgb(${dataArray[index]*0.8},8,179,1)`;
+      canvasCtx[indx].fillRect(x, y, barWidth, barHeight);
+
+      x += barWidth + barGap;
     }
     requestAnimationFrame(animate);
   }
   animate();
 }
-// canvas drawing end
 
-btnAudioPlay.forEach((button) => {
-  button.addEventListener("click", () => {
-    // 💥💥💥💥💥 button toggle function strat 💥💥💥💥💥💥💥
-    playPaseBtn();
-    // 💥💥💥💥💥 bytton toggle function end 💥💥💥💥💥💥💥
+btnAudio.forEach((btn, indx) =>
+  btn.addEventListener("click", () => {
+    // play pause button toggle func
+    handleBtnToggle(indx);
 
-    // get info which icon are shown prcess start
-    const btnAudioPlay_iTag = button.querySelectorAll("i");
-    btnAudioPlay_iTag.forEach((iTag) => {
-      if (!iTag.classList.contains("d-none")) {
-        // start the audio
-        audio.play();
+    // audio analization func
+    handleAnalization(indx);
 
-        // 💥💥💥💥💥 audio analyzer function strat 💥💥💥💥💥💥💥
-        audioInfo();
-        // 💥💥💥💥💥 audio analyzer function end 💥💥💥💥💥💥💥💥
-
-        // 💥💥💥💥💥 canvas draw function start 💥💥💥💥💥💥💥💥
-        drawCanvas();
-        // 💥💥💥💥💥 canvas draw function end 💥💥💥💥💥💥💥💥
-      } else {
-        // pause the audio
-        audio.pause();
-      }
-    });
-    // get info which icon are shown prcess end
-  });
-});
+    // drawing on canvas
+    handleDraw(indx);
+  })
+);
 
 // ✅Audio Visualizer canvas draw end
 
